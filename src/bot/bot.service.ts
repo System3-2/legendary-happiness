9d3f3af37af0ea0939914f06daa7d6efcd62800f
OlojaMuyiwa <olojam4969@gmail.com>
2023-06-02 00:32:15 +0100

    Add OpenAI module and create GPT-3 prompt-reply feature

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Once, InjectDiscordClient, On } from '@discord-nestjs/core';
 import { Client, Events } from 'discord.js';
+import { AiService } from 'src/ai/ai.service';
 
 @Injectable()
 export class BotService {
   constructor(
     @InjectDiscordClient()
     private readonly client: Client,
+    private aiService: AiService
   ) { }
 
   @Once('ready')
@@ -16,7 +18,16 @@ export class BotService {
 
   @On(Events.MessageCreate)
   async sendMessage(message) {
-    console.log(message.content)
+    if (message.content.substring(0, 1) === "!") {
+      const prompt = message.content.substring(1); //remove the exclamation mark from the message
+      const answer = await this.aiService.createCompletion(prompt) //prompt GPT-3
+      if (answer) {
+        message.reply(answer)
+      }
+      else {
+        message.reply(`Something went wrong`)
+      }
+    }
 
   }
 }
