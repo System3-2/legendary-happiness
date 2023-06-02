c021088abef1671ff507ec9aec3ba33fa91c87e5
OlojaMuyiwa <olojam4969@gmail.com>
2023-06-02 17:22:17 +0100

    fixing git

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DiscordModule } from '@discord-nestjs/core';
+<<<<<<< HEAD
 import { OpenAIModule } from '@platohq/nestjs-openai';
 import { GatewayIntentBits } from 'discord.js';
 import { BotModule } from './bot/bot.module';
 import { AiService } from './ai/ai.service';
 import { AiModule } from './ai/ai.module';
+=======
+import { GatewayIntentBits } from 'discord.js';
+import { BotModule } from './bot/bot.module';
+>>>>>>> 6cddd8f (discord initial setup)
 import * as dotenv from 'dotenv'
 dotenv.config()
 
@@ -24,6 +29,7 @@ dotenv.config()
         },
       }),
     }),
+<<<<<<< HEAD
     OpenAIModule.register({
       apiKey: process.env.api_key,
     }),
@@ -32,5 +38,11 @@ dotenv.config()
   ],
   controllers: [AppController],
   providers: [AppService, AiService],
+=======
+    BotModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+>>>>>>> 6cddd8f (discord initial setup)
 })
 export class AppModule { }
--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { Once, InjectDiscordClient, On } from '@discord-nestjs/core';
 import { Client, Events } from 'discord.js';
+<<<<<<< HEAD
 import { AiService } from 'src/ai/ai.service';
+=======
+>>>>>>> 6cddd8f (discord initial setup)
 
 @Injectable()
 export class BotService {
   constructor(
     @InjectDiscordClient()
     private readonly client: Client,
+<<<<<<< HEAD
     private aiService: AiService
+=======
+>>>>>>> 6cddd8f (discord initial setup)
   ) { }
 
   @Once('ready')
@@ -18,6 +24,7 @@ export class BotService {
 
   @On(Events.MessageCreate)
   async sendMessage(message) {
+<<<<<<< HEAD
     if (message.content.substring(0, 1) === "!") {
       const prompt = message.content.substring(1); //remove the exclamation mark from the message
       const answer = await this.aiService.createCompletion(prompt) //prompt GPT-3
@@ -28,6 +35,9 @@ export class BotService {
         message.reply(`Something went wrong`)
       }
     }
+=======
+    console.log(message.content)
+>>>>>>> 6cddd8f (discord initial setup)
 
   }
 }
